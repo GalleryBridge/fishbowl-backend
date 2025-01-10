51504846f2da3977bf4b79b7304c92c64f1f11ed
GalleryBridge <laidoring@163.com>
2025-01-10 10:05:02 +0800

    init

---
+++ src/main/java/com/laidor/fishbowlbackend/FishbowlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.laidor.fishbowlbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishbowlBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishbowlBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laidor/fishbowlbackend/FishbowlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laidor.fishbowlbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishbowlBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
